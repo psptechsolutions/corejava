42f0dc78a5535489d21a3f04304200a571cfa7c4
ananthi <psananthi@yahoo.com>
2024-01-11 13:17:38 +0530

    Added Collection List sample

---
+++ Collection/src/com/psp/ListSample.java
@@ -0,0 +1,10 @@
+package com.psp;
+
+public class ListSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Collection----List");
+	}
+
+}
