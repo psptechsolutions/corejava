f7ba92820b4fb8efba03529938939040e9564e83
ananthi <psananthi@yahoo.com>
2024-01-11 13:55:50 +0530

    Sample for HashMap

---
+++ Collection/src/com/psp/sampleHashMap/SampleHashMap.java
@@ -0,0 +1,10 @@
+package com.psp.sampleHashMap;
+
+public class SampleHashMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample for HashMap");
+	}
+
+}
