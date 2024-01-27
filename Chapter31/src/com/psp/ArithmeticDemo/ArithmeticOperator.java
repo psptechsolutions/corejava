5e3dbfaacc414525d048be29745f3733047db173
ananthi <psananthi@yahoo.com>
2024-01-27 14:29:14 +0530

    second commit

---
+++ Chapter31/src/com/psp/ArithmeticDemo/ArithmeticOperator.java
@@ -0,0 +1,22 @@
+package com.psp.ArithmeticDemo;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 5;
+		System.out.print( "Addition of two numbers (10,5) = " );
+		System.out.println( a  +  b );
+		System.out.print( "Subtraction of two numbers (10,5)= " );
+		System.out.println( a  -  b );	
+		System.out.print( "Multiplication of two numbers (10,5)= " );
+		System.out.println( a  *  b );	
+		System.out.print( "Division of two numbers (10,5)= " );
+		System.out.println( a  /  b );
+		System.out.print( "Modulus of two numbers (10,5)= " );
+		System.out.println( a  %  b );
+
+	}
+
+}
